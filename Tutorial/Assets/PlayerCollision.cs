215845d66a6351f009df1aeb933aff2a85a57892
zoro2002 <85740860+zoro2002@users.noreply.github.com>
2021-06-11 13:14:39 +0200

    Added Tutorial

---
+++ Tutorial/Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            playerMovement.enabled = false;
+        }
+    }
+}
+   
